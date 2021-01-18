1ece644ddc9fb4b5fc24b6b0c696b1ebf3ed924a
vierth <vierth@gmail.com>
2021-01-18 16:23:15 -0500

    Add files via upload

---
+++ fromzeroton/basic_python.py
@@ -0,0 +1,42 @@
+# Super basic python info
+
+# Intgers
+0
+1
+12
+8532
+-36
+
+# Floating point number
+12.3
+81.999999
+-12.4
+
+# String
+"Hello, my name is Paul!"
+'Hello, my name is Paul!'
+
+'"Hello!", he said!'
+"\"Hello!\", he said!"
+
+"It was the church\'s car"
+
+# Boolean
+True
+False
+
+# NoneType
+None
+
+# variables (avoid naming after reserved words)
+greeting_from_paul = "Hello, my name is Paul!"
+my_number = "120"
+my_list = "hello!"
+
+# functions execute some sort of command
+print(greeting_from_paul)
+print(my_number)
+
+print(type(my_number))
+
+
